4efd16fefdf8bb0cd60bb86503d4d4e0d410ff5c
BPL08 <locdm123@gmail.com>
2025-02-12 21:21:11 +0700

    clean main code

--- src/main/java/com/be/back_end/controller/MaterialController.java
+++ src/main/java/com/be/back_end/controller/MaterialController.java
@@ -1,9 +1,7 @@
 package com.be.back_end.controller;
 
 import com.be.back_end.service.ImplementService.MaterialService;
-import com.be.back_end.service.ImplementService.TshirtService;
 import com.be.back_end.service.InterfaceService.IMaterialService;
-import com.be.back_end.service.InterfaceService.ITshirtService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MaterialController {
