4043f23df42f2e8b73afb5dbef87720a24405c56
ultrakhaicraft <ultrakhaicraft2004@gmail.com>
2025-03-10 21:26:57 +0700

    Implement Paging in Get Transactions For System and For Customer

--- src/main/java/com/be/back_end/repository/TranscationRepository.java
+++ src/main/java/com/be/back_end/repository/TranscationRepository.java
@@ -3,12 +3,28 @@ package com.be.back_end.repository;
 
 import com.be.back_end.model.Bookings;
 import com.be.back_end.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TranscationRepository extends JpaRepository<Transaction, String> {
-    List<Transaction> findAllByBookings(Bookings booking);
+
+
+
+    @Query(value = "SELECT t.* FROM transaction t " +
+            "INNER JOIN bookings b ON t.bookingid = b.bookingid " +
+            "WHERE b.accountid = :accountId " +
+            "ORDER BY t.transactiondate",
+            countQuery = "SELECT COUNT(t.transactionid) FROM transaction t " +
+                    "INNER JOIN bookings b ON t.bookingid = b.bookingid " +
+                    "WHERE b.accountid = :accountId",
+            nativeQuery = true)
+    Page<Transaction> findAllByAccountId(@Param("accountId") String accountId, Pageable pageable);
+    Page<Transaction> findAll(Pageable pageable);
 }
--- src/main/java/com/be/back_end/service/TranscationService/ITranscationService.java
+++ src/main/java/com/be/back_end/service/TranscationService/ITranscationService.java
@@ -2,15 +2,16 @@
 package com.be.back_end.service.TranscationService;
 
 import com.be.back_end.dto.TransactionDTO;
+import com.be.back_end.dto.response.PaginatedResponseDTO;
 import com.be.back_end.dto.response.TransactionDetailResponse;
 
 import java.util.List;
 
 public interface ITranscationService {
     TransactionDTO create(TransactionDTO user);
-    List<TransactionDTO> getAll();
+    PaginatedResponseDTO<TransactionDTO> getAll(int page, int size, String sortDir, String sortBy);
     TransactionDTO getById(String id);
-    List<TransactionDTO> getAllForCustomer(String id);
+    PaginatedResponseDTO<TransactionDTO> getAllByCustomer(String customerId, int page, int size, String sortDir, String sortBy);
     TransactionDetailResponse getTransactionDetail(String id);
 }
 
